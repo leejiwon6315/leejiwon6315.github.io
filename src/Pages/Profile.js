fc7d17c448822ef9149f9d1ea7197418d3e44c57
leejiwon6315 <leejiwon6315@naver.com>
2020-10-28 15:58:08 +0900

    update profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -27,26 +27,44 @@ const Profile = ({ name }) => {
         </h1>
         <span>-<br/><br/>Personal Profile</span>
         <div className={styles.profile_main_article}>
-          <ul>
+          <ul className={styles.profile_left_subtitle}>
             <li>Name</li>
             <li>Age</li>
             <li>Position</li>
             <li>Tech Stack<br/><br/><br/></li>
-            <li>Major<br/><br/></li>
-            
             <li>Location</li>
-
+            <li>Career<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></li>
             <li>E-mail</li>
             <li>Github</li>
           </ul>
 
-          <ul>
+          <ul className={styles.profile_right_subtitle}>
             <li>JEEWON LEE</li>
             <li>24</li>
             <li>Web Front-end</li>
             <li>HTML, CSS, Javascript<br/>React js<br/>Photoshop, Illustrator, Figma</li>
-            <li>Global Media,<br/>IT College of Soongsil Univ.</li>
             <li>Seoul, Republic Of Korea</li>
+            <li>
+              2015.03 ~<br/>
+              Major in Global-Media, IT college of Soongsil University<br/>
+
+              <br/>
+              2017.01 ~ 2018.10<br/>
+              Served in the Republic of Korea Special Assault Commando<br/>
+
+              <br/>
+              2019.03 ~ 2020.01<br/>
+              Working Holiday in Osaka, Japan<br/>
+              Main Designer of S.Y.Company in Osaka, Japan<br/>
+
+              <br/>
+              2020.02 ~ <br/>
+              Operating Hand-made Accessory Brand A.ELO<br/>
+
+              <br/>
+              2020.07 ~ <br/>
+              Beginning to study Web Front-end<br/>
+            </li>
             <li>leejiwon6315@naver.com</li>
             <li><a href = "https://github.com/leejiwon6315">github.com/leejiwon6315</a></li>
           </ul>
--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -24,8 +24,12 @@ const WelcomePage = ({ name, onChangeName, handleSubmit }) => {
         <p className={styles.main_explain}>
           This website is just my portfolio website. If you are curious about
           me, please enter this website. <br />
-          There are no viruses or download files on this website, so be assured.{" "}
-          Let's enter together now.
+          There are no viruses or download files on this website, so be assured.
+          Let's enter together now!
+        </p>
+        <p className={styles.main_explain}>
+          이 웹사이트는 저의 포트폴리오 웹사이트 입니다. 저에 대해 궁금하시다면, 이 웹사이트에 접속해 주세요. <br />
+          바이러스 혹은 그에 준하는 파일이 다운로드되거나 하지 않으니 안심하세요. 그럼 함께 접속해 봅시다!
         </p>
         <h3 className={styles.user_question}> What is your name?</h3>
         <input
